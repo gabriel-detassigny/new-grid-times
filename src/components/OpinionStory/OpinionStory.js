237b616a673e0b267fc5b2925fd694404f5b6f41
Gabriel de Tassigny <gabriel@multivac.cc>
2022-07-14 14:20:03 +0900

    Align opinion's avatar

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import {QUERIES} from '../../constants';
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
@@ -24,6 +25,12 @@ const Link = styled.a`
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: column;
+  }
 `;
 
 const Avatar = styled.img`
