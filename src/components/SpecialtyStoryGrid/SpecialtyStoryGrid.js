6851bf48b46f35c23f4f64f6bda8cfe971abf7f1
Gabriel de Tassigny <gabriel@multivac.cc>
2022-07-15 13:09:50 +0900

    Fix speciality story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import {QUERIES} from '../../constants';
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -47,14 +48,53 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  display: grid;
+  grid-template-columns: minmax(0, 1fr);
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+    grid-template-rows: 52px 1fr;
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 16px;
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: repeat(4, 1fr);
+  }
 
-const SportsSection = styled.section``;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+  display: grid;
+  grid-template-columns: minmax(0, 1fr);
+  @media ${QUERIES.laptopAndUp} {
+    padding-left: 16px;
+  }
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 16px;
+  @media ${QUERIES.tabletAndUp} {
+    grid-auto-flow: column;
+    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+    grid-auto-columns: minmax(220px,1fr);
+    overflow-x: auto;
+  }
+`;
 
 export default SpecialtyStoryGrid;
